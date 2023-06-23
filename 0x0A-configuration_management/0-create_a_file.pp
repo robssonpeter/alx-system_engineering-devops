b65b0fe579d5871e3870ef170b5768b9f18f2e70
Peter Mgembe <peterrobsson@gmail.com>
2023-06-23 03:35:03 -0700

    Solved the zeroth question

---
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -0,0 +1,8 @@
+# Script to create a file in puppet
+file { '/tmp/school':
+  ensure  => present,
+  mode    => '0744',
+  owner   => 'www-data',
+  group   => 'www-data',
+  content => 'I love Puppet',
+}
---
+++ 0x0A-configuration_management/README.md
@@ -0,0 +1,1 @@
+The puppet project for configuration management
