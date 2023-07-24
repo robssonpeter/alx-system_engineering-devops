12def6061262d310ebdfb557b017570f5d29738c
Peter Mgembe <peterrobsson@gmail.com>
2023-07-24 14:55:38 +0300

    solved the zeroth question

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -6,20 +6,24 @@ Script to make a request to an API
 import requests
 from sys import argv
 
-try:
-    protocal = "https://"
-    domain = "my-json-server.typicode.com"
-    uri = f"/robssonpeter/alx-system_engineering-devops/employees/{argv[1]}"
-    url = protocal+domain+uri
-    with requests.get(url) as req:
-        result = req.json()
-        if len(result) > 0:
-            length = result['total']
-            is_done = "is done with tasks"
-            tasks = result['tasks']
-            text = f"{result['name']} {is_done}({len(tasks)}/{length}):"
+url = 'https://jsonplaceholder.typicode.com/todos'
+users_url = f"https://jsonplaceholder.typicode.com/users/{argv[1]}"
+with requests.get(users_url) as usr_resp:
+    name = usr_resp.json()['name']
+    is_done = "is done with tasks"
+    with requests.get(url) as resp:
+        completed = []
+        total = 0
+        user_id = argv[1]
+
+        for todo in resp.json():
+            if todo['userId'] == int(user_id):
+                total += 1
+                if todo['completed'] is True:
+                    completed.append(todo)
+        if len(completed) > 0:
+            text = f"Employee {name} {is_done}({len(completed)}/{total}):"
             print(text)
-            for task in req.json()['tasks']:
-                print("\t"+task)
-except IndexError:
-    print('there is an error')
+            for task in completed:
+                print("\t"+task['title'])
+
