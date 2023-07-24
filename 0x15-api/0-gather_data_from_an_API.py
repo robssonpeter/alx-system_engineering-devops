2d2578228ea1c9b4169880629bbe8c2bfcceca79
Peter Mgembe <peterrobsson@gmail.com>
2023-07-24 15:29:32 +0300

    solved zeroth question

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -7,7 +7,7 @@ from sys import argv
 if __name__ == "__main__":
     url = 'https://jsonplaceholder.typicode.com/todos'
     users_url = f"https://jsonplaceholder.typicode.com/users/{argv[1]}"
-    
+ 
     """ Run only if this is the main module """
     with requests.get(users_url) as usr_resp:
         name = usr_resp.json()['name']
