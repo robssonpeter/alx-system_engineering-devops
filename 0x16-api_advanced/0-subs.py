d096fc7b20813e9bf98208d675be651f6c8da4e8
Peter Mgembe <peterrobsson@gmail.com>
2023-08-09 05:32:43 +0300

    solved the first question

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -3,12 +3,14 @@
 
 import requests
 
+
 def number_of_subscribers(subreddit):
     """ Set a custom User-Agent to avoid Too Many Requests error """
     headers = {'User-Agent': 'Custom User-Agent'}
 
     """ Make the API request to get subreddit information """
-    response = requests.get(f"https://www.reddit.com/r/{subreddit}/about.json", headers=headers)
+    url = f"https://www.reddit.com/r/{subreddit}/about.json"
+    response = requests.get(url, headers=headers)
 
     """ Check if the request was successful """
     if response.status_code == 200:
@@ -21,4 +23,4 @@ def number_of_subscribers(subreddit):
     elif response.status_code == 404:
         return 0
     else:
-        return 0  
+        return 0
