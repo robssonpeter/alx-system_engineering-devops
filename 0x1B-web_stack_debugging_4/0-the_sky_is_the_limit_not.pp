e873f45a9841fa462ca65593ebe09b14a93d484e
robssonpeter <peterrobsson@gmail.com>
2023-08-18 02:52:35 +0000

    solved the webstack debugging 4

---
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -0,0 +1,13 @@
+# Increases the limit of traffic that can be handled by Nginx server
+
+# Increase the ULIMIT for the default file
+exec { 'fix--for-nginx':
+  command => 'sed -i "s/15/4096/" /etc/default/nginx',
+  path    => '/usr/local/bin/:/bin/'
+} ->
+
+# Restart Nginx
+exec { 'nginx-restart':
+  command => 'nginx restart',
+  path    => '/etc/init.d/'
+}
